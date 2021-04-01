c660039fd4acc30316efec2229468d7a5d39c071
arnophpc <arnophpc@gmail.com>
2021-04-01 10:25:38 +0200

    Redirect to correct landing page

--- src/main/java/com/numberone/webshop/controller/HomeController.java
+++ src/main/java/com/numberone/webshop/controller/HomeController.java
@@ -3,10 +3,13 @@ package com.numberone.webshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index(){
-        return "forward:/index.html";
+    public void index(HttpServletResponse response){
+        response.setHeader("Location", "http://landingpage-ucllteam01.ocp-ucll-40cb0df2b03969eabb3fac6e80373775-0000.eu-de.containers.appdomain.cloud/");
+        response.setStatus(302);
     }
 }
